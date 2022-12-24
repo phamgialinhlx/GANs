24dbabb9c9b3ab0ceb6480ea09e476d9272a5cae
phamgialinhlx <phamgialinhlx2@gmail.com>
2022-12-24 13:28:23 +0700

    Test code quality of discriminator.py

--- src/models/gan/discriminator.py
+++ src/models/gan/discriminator.py
@@ -1,9 +1,7 @@
-from torch import nn;
+from torch import nn
 
 def get_discriminator_block(input_dim, output_dim):
-    '''
-    Discriminator Block
-    Function for returning a neural network of the discriminator given input and output dimensions.
+    """Function for returning a neural network of the discriminator block given input and output dimensions.
     Parameters:
         input_dim: the dimension of the input vector, a scalar
         output_dim: the dimension of the output vector, a scalar
@@ -11,35 +9,31 @@ def get_discriminator_block(input_dim, output_dim):
         a discriminator neural network layer, with a linear transformation 
           followed by an nn.LeakyReLU activation with negative slope of 0.2 
           (https://pytorch.org/docs/master/generated/torch.nn.LeakyReLU.html)
-    '''
-    return nn.Sequential(
-        nn.Linear(input_dim, output_dim),
-        nn.LeakyReLU(negative_slope=0.2)
-    )
+    """
+    return nn.Sequential(nn.Linear(input_dim, output_dim), nn.LeakyReLU(negative_slope=0.2))
 
 class Discriminator(nn.Module):
-    '''
+    """
     Discriminator Class
     Values:
         im_dim: the dimension of the images, fitted for the dataset used, a scalar
             (MNIST images are 28x28 = 784: default)
         hidden_dim: the inner dimension, a scalar
-    '''
+    """
     def __init__(self, im_dim=784, hidden_dim=128):
         super(Discriminator, self).__init__()
         self.disc = nn.Sequential(
             get_discriminator_block(im_dim, hidden_dim * 4),
             get_discriminator_block(hidden_dim * 4, hidden_dim * 2),
             get_discriminator_block(hidden_dim * 2, hidden_dim),
-            nn.Linear(hidden_dim, 1)
+            nn.Linear(hidden_dim, 1),
         )
     def forward(self, image):
-        '''
-        Function for completing a forward pass of the discriminator: Given an image tensor, 
+        """Function for completing a forward pass of the discriminator: Given an image tensor, 
         returns a 1-dimension tensor representing fake/real.
         Parameters:
             image: a flattened image tensor with dimension (im_dim)
-        '''
+        """
         return self.disc(image)
 
 if __name__ == "__main__":
