52a178ffc54dfb5a8298d0588b857f8cacb560bd
phamgialinhlx <phamgialinhlx2@gmail.com>
2023-03-20 20:51:08 +0700

    gender dataset

--- src/utils/callback/WandbCallback.py
+++ src/utils/callback/WandbCallback.py
@@ -6,40 +6,29 @@ import pytorch_lightning as pl
 from typing import Any
 from torchmetrics.image.fid import FrechetInceptionDistance
 import cv2
-from src.utils.gan_utils import get_noise, get_one_hot_labels, combine_vectors
+from src.utils.gan_utils import get_noise
 
 class WandbCallback(Callback):
-    def __init__(self, z_dim:int = 64, batch_size=128, im_chan:int = 1, n_classes:int = 10, img_size:int = 28, use_fixed_noise:bool = True):
+    def __init__(self, z_dim:int = 64, batch_size=128, im_chan:int = 1, img_size:int = 28, use_fixed_noise: bool = False):
         self.size = [im_chan, img_size, img_size]
         # self.size = (1, 28, 28)
         self.z_dim = z_dim #z_dim
         self.batch_size = batch_size
         self.step = 500
-        self.n_classes = n_classes
         self.last_batch = None
         self.fid = FrechetInceptionDistance(feature=64)
-        self.use_fixed_noise = use_fixed_noise
-        self.fake_noise = get_noise(self.batch_size, self.z_dim, device = 'cuda') # get noise
 
     def on_train_epoch_end(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule"):
-        if (len(self.last_batch) == 2):
-            last_batch, labels = self.last_batch
-        else:
-            last_batch = self.last_batch
-
-        if (not self.use_fixed_noise):
-            self.fake_noise = get_noise(len(last_batch), self.z_dim, device = 'cuda')
-
-        one_hot_labels = get_one_hot_labels(labels, self.n_classes)
-        noise_and_labels = combine_vectors(self.fake_noise, one_hot_labels)
-
-        fake = trainer.model(noise_and_labels)
+        fake_noise = get_noise(self.batch_size, self.z_dim, device = 'cuda') # get noise
+        # get the image from your model or dataloader
+        # with trainer.model.eval():
+        fake = trainer.model(fake_noise)
         fake = fake.detach()
         
         image_unflat = fake.detach().cpu().view(-1, * self.size)
         image_grid = make_grid(image_unflat[:25], nrow=5)
 
-        real_unflat = last_batch.view(-1, * self.size)
+        real_unflat = self.last_batch.view(-1, * self.size)
         real_grid = make_grid(real_unflat[:25], nrow=5)
 
         # logging using WandB
@@ -48,4 +37,7 @@ class WandbCallback(Callback):
         
 
     def on_train_batch_end(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule", outputs: Any, batch: Any, batch_idx: int) -> None:
-        self.last_batch = batch
+        if (len(batch) == 2):
+            self.last_batch, _ = batch
+        else:
+            self.last_batch = batch
